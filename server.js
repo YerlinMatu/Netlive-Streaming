ac18fbe904713d5771e3f1af80e63dd00f4d9413
Yerlin Matu <maicolmatu27@gmail.com>
2018-09-09 11:52:12 -0500

    Update server.js

--- server.js
+++ server.js
@@ -9,22 +9,22 @@ const io = require('socket.io')(http);
 const PORT = 9009;
 
 const Log = require('log');
-let   log = new Log('debug')
+let log = new Log('debug');
 
 app.use('/', express.static(__dirname + '/public'));
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
    res.redirect('index.html');
 });
 
 // Escuchar conexión de usuario.
-io.on('connection', (socket)=>{
-   socket.on('stream', (imagen)=>{
+io.on('connection', (socket) => {
+   socket.on('stream', (imagen) => {
    socket.broadcast.emit('stream', imagen);
   })
 })
 
-http.listen(PORT, ()=>{
+http.listen(PORT, () => {
    log.info(`\n Servidor ${hostname} arrancado en el puerto ${PORT}`);
 });
 
